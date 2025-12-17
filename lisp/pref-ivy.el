c77f0fec562d7239d4e6dec2bef37020f5193098
murry2018 <2clean8@naver.com>
2025-12-17 19:59:33 +0900

    Change ivy and swiper keybindings

--- lisp/pref-ivy.el
+++ lisp/pref-ivy.el
@@ -8,10 +8,11 @@
   :bind ("C-c C-r" . ivy-resume)
   :bind (:map ivy-minibuffer-map
          ("<return>" . ivy-alt-done)
-         ("M-j" . ivy-yank-symbol))
+         ("C-s" . swiper)
+         ("C-r" . swiper))
   :bind (:map ivy-occur-grep-mode-map
-         ("C-n" . next-error-no-select)
-         ("C-p" . previous-error-no-select))
+          ("C-n" . next-error)
+          ("C-p" . previous-error))
   :config
   (setopt ivy-use-virtual-buffers 'recentf
         ivy-magic-tilde nil
@@ -27,6 +28,7 @@
 
 (use-package swiper :ensure t
   :bind (("C-s" . swiper)
+         ("C-r" . swiper)
          ("C-M-s" . swiper-all)))
 
 (provide 'pref-ivy)
