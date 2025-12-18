88627a9e55feff6cb1db2b4d7b955f62d7dc745b
murry2018 <2clean8@naver.com>
2025-12-18 21:12:08 +0900

    Implement custom c-ts-mode indentation

--- lisp/pref-lang-c.el
+++ lisp/pref-lang-c.el
@@ -75,19 +75,72 @@ patterns (see https://github.com/clangd/clangd/issues/719).")))))
 (use-package lsp-mode
   :hook ((c++-mode c-mode c-ts-mode c++-ts-mode) . lsp))
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; Syntax-aware Indentation Configuration
+;;
+
+;; CC-mode configuration
 (c-add-style "pref/c"
-             '("k&r"
+             '("bsd"
                (c-basic-offset . 4)
                (c-offsets-alist
                 (cpp-macro . 0)
                 (arglist-close . c-lineup-close-paren))))
 (setopt c-default-style "pref/c")
 
+;; Ts-mode configuration
+(defvar c-ts-mode-indent-style)
+(declare-function treesit-node-at "treesit" (point))
+(declare-function treesit-node-type "treesit.c" (node))
+(declare-function c-ts-mode--indent-styles "c-ts-mode" (mode))
+
+(defun pref.style/pref-lang-c ()
+  "Define a customized indentation style based on BSD."
+  (let* ((mode (if (derived-mode-p 'c-ts-mode) 'c 'c++))
+         (bsd-style (alist-get 'bsd (c-ts-mode--indent-styles mode))))
+    `(;;; Rules for parameter list for function declaration
+      ((match nil "parameter_list" nil 1 1) standalone-parent c-ts-mode-indent-offset)
+      ((match ")" "parameter_list" nil nil nil) standalone-parent 0)
+      ((match nil "parameter_list" nil 2 nil) (nth-sibling 1) 0)
+      ((and no-node (parent-is "parameter_list")) (nth-sibling 1) 0)
+      
+      ;;; Rules for argument list for function invocation
+      ((node-is ")") parent-bol 0)
+      ((match nil "argument_list" nil 1 1) parent-bol c-ts-mode-indent-offset)
+      ((parent-is "argument_list") prev-sibling 0)
+      ((match nil "parameter_list" nil 1 1) parent-bol c-ts-mode-indent-offset)
+      ((parent-is "parameter_list") prev-sibling 0)
+      
+      ;;; Rules for preprocessors conditionals
+      ((and (node-is "preproc.*")) no-indent 0)
+      ((node-is "#endif") no-indent 0)
+      
+      ;;; Derived from BSD
+      ,@bsd-style)))
+
+(defun pref.inner/c-ts-mode-maybe-literal-tab ()
+  "Insert a literal tab for preprocessor directives, otherwise indent."
+  (interactive)
+  (if (and (eq c-ts-mode-indent-style #'pref.style/pref-lang-c)
+           (or (save-excursion (back-to-indentation)
+                               (looking-at-p "#"))
+               (when-let ((node (treesit-node-at (point))))
+                 (equal (treesit-node-type node)
+                        "preproc_arg"))))
+      (tab-to-tab-stop)
+    (call-interactively #'indent-for-tab-command)))
+
+(defun pref.inner/c-ts-mode-hook ()
+  "Configure indentation style and keybindings for `c-ts-mode'."
+  (setopt c-ts-mode-indent-style #'pref.style/pref-lang-c)
+  (local-set-key (kbd "TAB") #'pref.inner/c-ts-mode-maybe-literal-tab)
+  (local-set-key (kbd "<tab>") #'pref.inner/c-ts-mode-maybe-literal-tab))
+
 (use-package c-ts-mode
   :defer t
+  :hook ((c-ts-mode c++-ts-mode) . pref.inner/c-ts-mode-hook)
   :config
-  (setopt c-ts-mode-indent-offset 4
-          c-ts-mode-indent-style 'k&r))
+  (setopt c-ts-mode-indent-offset 4))
 
 (provide 'pref-lang-c)
 ;;; pref-lang-c.el ends here
