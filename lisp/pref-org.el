2159404a43e45bbb3c63b6db277ead8bd679149c
murry2018 <2clean8@naver.com>
2026-01-30 23:24:33 +0900

    (org) Revert "Fix syntax handling of angle brackets in Org source blocks"
    The change relied on a custom syntax-propertize-function override,
    which bypassed Org's native syntax handling.  Revert this commit to
    restore the default Org behavior and avoid maintenance overhead.

--- lisp/pref-org.el
+++ lisp/pref-org.el
@@ -27,31 +27,6 @@
   (with-eval-after-load 'company
     (add-hook 'company-mode-hook #'pref.inner/org-disable-company-dabbrev)))
 
-;; Treat angle brackets as symbol constituents in Org source blocks
-;; See: https://emacs.stackexchange.com/a/52209
-;; Adapted from a solution by @john-kitchin.
-(defun org-mode-<>-syntax-fix (start end)
-  "Mark characters `<' and `>' as symbol constituents inside org src blocks.
-This function is suitable for `syntax-propertize-function' and must
-only operate on the region from START to END."
-  (save-excursion
-    (goto-char start)
-    (while (re-search-forward "[<>]" end t)
-      (let ((pos (match-beginning 0)))
-        (goto-char pos)
-        (when (org-in-src-block-p)
-          (put-text-property pos (1+ pos) 'syntax-table (string-to-syntax "_")))
-        (goto-char (1+ pos))))))
-
-(defun org-setup-<>-syntax-fix ()
-  "Set up `syntax-propertize-function' for `org-mode' buffers."
-  (setq-local syntax-propertize-function #'org-mode-<>-syntax-fix)
-  ;; If you want existing buffer text to be propertized immediately, uncomment:
-  ;; (syntax-propertize (point-max))
-  )
-
-(add-hook 'org-mode-hook #'org-setup-<>-syntax-fix)
-
 (provide 'pref-org)
 ;;; pref-org.el ends here
 
