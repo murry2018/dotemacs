f7715990730f65d9120ff639299c628a18e0957a
murry2018 <2clean8@naver.com>
2026-08-03 06:01:04 +0000

    (dependency, compatibility) Address package-Emacs version compatibility issues
    - (early-init) prefer newer *.el file
    - (init) load latest compat when available

--- early-init.el
+++ early-init.el
@@ -3,6 +3,7 @@
 ;;; Code:
 (setopt gc-cons-threshold 100000000) ; ~100 MB
 (setq gc-cons-percentage 0.6)
+(setq load-prefer-newer t)
 
 (provide 'early-init)
 ;;; early-init.el ends here
--- init.el
+++ init.el
@@ -6,6 +6,13 @@
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
+;; Compatibility packages are sometimes required because maintainers use
+;; development versions of Emacs. In these instances, simply install the
+;; latest compat package (M-x package-install RET compat RET).
+(when (and (version< emacs-version "31")
+           (package-installed-p "compat"))
+  (require 'compat nil t))
+
 ;; Custom file
 (setopt custom-file (expand-file-name "custom.el" user-emacs-directory))
 (when (file-exists-p custom-file)
